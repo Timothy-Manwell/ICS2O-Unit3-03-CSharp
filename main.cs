970d16561d5e673ce8a6a7ec9eb8a2797627375b
Timothy-Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-11-02 14:29:27 -0400

    c#

--- main.cs
+++ main.cs
@@ -15,7 +15,7 @@ class Program
         Console.WriteLine("This program calculates the volume of a pyramid");
         Console.Write("\nEnter radius a in cm: ");
         radius = Convert.ToInt32(Console.ReadLine());
-        volume = (4.0 * Math.PI * Math.Pow (radius, 3)) / 3.0;
+        volume = (4.0 * Math.PI * Math.Pow(radius, 3)) / 3.0;
         Console.WriteLine("\nThe volume is: " + volume.ToString("0.00") + " cm³. ");
     }
 }
