e8b6c06a2f40476f27dd05fe4dc67976434eb019
Timothy-Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-11-02 18:23:48 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,21 @@
+// Created by: Timothy Manwell
+// Created on: Oct 2022
+//
+// This program calculates the area of a pyramid
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        double radius;
+        double volume;
+
+        Console.WriteLine("This program calculates the volume of a pyramid");
+        Console.Write("\nEnter radius a in cm: ");
+        radius = Convert.ToInt32(Console.ReadLine());
+        volume = (4.0 * Math.PI * Math.Pow (radius, 3)) / 3.0;
+        Console.WriteLine("\nThe volume is: " + volume.ToString("0.00") + " cm³. ");
+    }
+}
